39982965a120372a87f85fbd3514144162f635ec
Gnusson.net <spel@gnusson.net>
2020-12-15 10:36:41 +0100

    Add project files.

---
+++ OOP Uppgift/Cirkel.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP_Uppgift
+{
+    class Cirkel
+    {
+        public int radie;
+        public Cirkel(int r)
+        {
+            radie = r;
+        }
+
+        public void Area()
+        {
+            double area = math.ou
+        }
+
+        public void Omkrets()
+        {
+
+        }
+    }
+}
---
+++ OOP Uppgift/Cylinder.cs
@@ -0,0 +1,21 @@
+﻿namespace OOP_Uppgift
+{
+    class Cylinder : Cirkel
+    {
+        public int höjd;
+        public Cylinder(int radie, int h) : base(radie)
+        {
+            höjd = h;
+        }
+
+        public void Volym()
+        {
+
+        }
+
+        public void Yta()
+        {
+
+        }
+    }
+}
